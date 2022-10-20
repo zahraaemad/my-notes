9e9833ec944dc23249e16b89f2b31265b5c43607
Zooz <zahraa95emad@gmail.com>
2022-10-20 11:16:49 +0200

    styleedit

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,7 +7,9 @@ function Note(props) {
 
 
   return (
+
     <div className="note">
+    
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button onClick={() => { props.toDelete(props.id) }}> <DeleteIcon /></button>
